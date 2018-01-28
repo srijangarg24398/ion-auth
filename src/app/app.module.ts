0b5c17cafa81c94a8cdb20cc369f68be88797ad5
Srijan <srijan@localhost.localdomain>
2018-01-28 22:52:49 +0530

    Google and facebook login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+var config = {
+
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +20,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(config)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,65 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import firebase from 'firebase';
+// import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  // facebookLoggedIn:boolean=false;
+  provider={
+  	loggedIn:false,
+  	name:"",
+  	email:"",
+  	profilePicture:"",
+  	provider:""
+  }
+  // facebook:any;
+  constructor(public navCtrl: NavController,private afAuth:AngularFireAuth) {
 
   }
+  loginWithFacebook(){
+  	// var provider = new firebase.auth.FacebookAuthProvider();
+	// provider.addScope('user_birthday');
+	// firebase.auth().signInWithRedirect(provider);
+  	//this.afAuth.auth.signInWithRedirect(provider)
 
+  	this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+  	.then(res=>{
+  		// console.log(res.user.providerData);
+  		this.provider.loggedIn=true;
+  		this.provider.name=res.user.displayName;
+  		this.provider.email=res.user.email;
+  		this.provider.profilePicture=res.user.photoURL;
+  		this.provider.provider="facebook";
+  		// this.facebook=res.user.providerData[0];
+  		// this.facebookLoggedIn=true;
+  		// console.log(this.facebook);
+  	})
+  	.catch(err=>console.log(err));
+  }
+  logoutOfFacebook(){
+  	this.afAuth.auth.signOut();
+  	// this.facebookLoggedIn=false;
+  	this.provider.loggedIn=false;
+  }
+
+  loginWithGoogle(){
+  	this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+  	.then(res=>{
+  		console.log("Login via google");
+  		this.provider.loggedIn=true;
+  		this.provider.name=res.user.displayName;
+  		this.provider.email=res.user.email;
+  		this.provider.profilePicture=res.user.photoURL;
+  		this.provider.provider="Google";
+  	}).catch(err=>console.log(err));
+  }
+  logout(){
+  	this.afAuth.auth.signOut();
+  	this.provider.loggedIn=false;
+  }
 }
